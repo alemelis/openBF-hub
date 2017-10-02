2d671e4a598878de6067889718cdd44d1ba4adc6
ale <alessandro_melis@rocketmail.com>
2017-10-02 15:48:18 +0100

    Add ADAN56 model

--- models/alastruey2007modelling/plot.py
+++ models/alastruey2007modelling/plot.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-a_name = "alastruey2007modelling_results/23-L-MCA_"
+a_names = [#"alastruey2007modelling_results-normal/23-L-MCA_",
+            #"alastruey2007modelling_results-twice/23-L-MCA_",
+            "alastruey2007modelling_results/23-L-MCA_"]
 
 sns.set_style("ticks")
 fig1 = plt.figure(1)
@@ -19,25 +21,29 @@ sns.despine()
 
 titles = ["Cardiac cycle - Right middle cerebral artery",
             "Convergence history - Right middle cerebral artery"]
-for axs, ext, title in zip([[ax1, ax2], [ax3, ax4]], [".last", ".out"], titles):
-    for q, ax, m, lbl in zip(["P", "Q"], axs, [133.332, 1e-6], ["$P$ (mmHg)",
-                            "$Q$ (ml$\cdot$s$^{-1}$)"]):
+styles = ['-', '--', '.-']
+colors = ["crimson", "steelblue", "seagreen"]
+labels = ["$1\,$mm", "$0.5\,$mm", "$2\,$mm"]
+for a_name, style, color in zip(a_names, styles, colors):
+    for axs, ext, title in zip([[ax1, ax2], [ax3, ax4]], [".last", ".out"], titles):
+        for q, ax, m, lbl in zip(["P", "Q"], axs, [133.332, 1e-6], ["$P$ (mmHg)",
+                                "$Q$ (ml$\cdot$s$^{-1}$)"]):
 
-        w = np.loadtxt("{0}{1}{2}".format(a_name, q, ext))
+            w = np.loadtxt("{0}{1}{2}".format(a_name, q, ext))
 
-        if title == "Cardiac cycle":
-            w[:,0] -= w[0,0]
+            if title == "Cardiac cycle":
+                w[:,0] -= w[0,0]
 
-        ax.plot(w[:,0], w[:,3]/m)
-        ax.set_ylabel(lbl)
+            ax.plot(w[:,0], w[:,3]/m, style, color=color)
+            ax.set_ylabel(lbl)
 
-        if q == "Q":
-            ax.set_xlabel("time (t)")
-        else:
-            ax.set_title(title)
+            if q == "Q":
+                ax.set_xlabel("time (t)")
+            else:
+                ax.set_title(title)
 
 plt.draw()
 plt.show()
 
-fig1.savefig("imgs/{}.png".format(titles[0].split("-")[0].replace(" ","")))
-fig2.savefig("imgs/{}.png".format(titles[1].split("-")[0].replace(" ","")))
+# fig1.savefig("imgs/{}.png".format(titles[0].split("-")[0].replace(" ","")))
+# fig2.savefig("imgs/{}.png".format(titles[1].split("-")[0].replace(" ","")))
---
+++ models/boilleau2015benchmark/adan56/README.md
@@ -0,0 +1,9 @@
+# ADAN56
+
+_Blanco PJ, Watanabe SM, Passos MA, Lemos PA, Feijóo RA. An anatomically detailed arterial network model for one-dimensional computational hemodynamics. IEEE Transactions on biomedical engineering. 2015 Feb;62(2):736-53._
+
+---
+
+![img](imgs/Cardiaccycle.png)
+
+![img](imgs/Convergencehistory.png)
